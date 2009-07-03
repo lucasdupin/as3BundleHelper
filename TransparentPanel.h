4a2b703c2b7e9c08d468b69af46183cf89d11d7a
Lucas Dupin <lucas@lucasdupin.com>
2009-07-03 11:44:40 -0300

    playing with the interface

---
+++ TransparentPanel.h
@@ -0,0 +1,16 @@
+//
+//  TransparentPanel.h
+//  as3Debugger
+//
+//  Created by Lucas Dupin on 7/3/09.
+//  Copyright 2009 Gringo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TransparentPanel : NSPanel {
+
+}
+
+@end

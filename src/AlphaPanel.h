c5cf9cf8bc6e20b365ad550cdbb049e32dfee92b
Lucas Dupin <lucasdupin@Gauss.local>
2009-07-04 15:44:34 -0300

    changing alphaPanel folder

---
+++ src/AlphaPanel.h
@@ -0,0 +1,16 @@
+//
+//  AlphaPanel.h
+//  as3Debugger
+//
+//  Created by Lucas Dupin on 04/07/09.
+//  Copyright 2009 28.room. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AlphaPanel : NSPanel {
+
+}
+
+@end

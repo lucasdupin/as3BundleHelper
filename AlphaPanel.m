e0f6f69daca16b1c599ae279b6dc13a2851ca10d
Lucas Dupin <lucasdupin@Gauss.local>
2009-07-04 15:43:04 -0300

    adding panel subclass, changing buttons copy and place

---
+++ AlphaPanel.m
@@ -0,0 +1,27 @@
+//
+//  AlphaPanel.m
+//  as3Debugger
+//
+//  Created by Lucas Dupin on 04/07/09.
+//  Copyright 2009 28.room. All rights reserved.
+//
+
+#import "AlphaPanel.h"
+
+
+@implementation AlphaPanel
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+		NSLog(@"init!");
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+    // Drawing code here.
+}
+
+@end

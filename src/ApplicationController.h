834c231a69fbbf38353eb1a40e8bb82be6c04d7e
Lucas Dupin <lucas@lucasdupin.com>
2009-08-17 19:06:30 -0300

    cleaning up the house and adding regex framework

---
+++ src/ApplicationController.h
@@ -0,0 +1,25 @@
+//
+//  ApplicationController.h
+//  as3Debugger
+//
+//  Created by Lucas Dupin on 8/12/09.
+//  Copyright 2009 Gringo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "TraceController.h"
+#import "DebuggingViewController.h"
+
+
+@interface ApplicationController : NSObject {
+	IBOutlet TraceController *traceController;
+	IBOutlet DebuggingViewController *debuggingViewController;
+	
+	IBOutlet NSWindow *debugWindow;
+}
+
+- (IBAction) showLogViewer: (id) sender;
+- (IBAction) showDebuggingView: (id) sender;
+
+@end
+
